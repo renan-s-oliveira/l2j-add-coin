534255b70ecb1611f81e070035ddf0d38d35133d
Renan de Souza Oliveira <renanoliveira1988@gmail.com>
2021-02-05 16:44:26 -0300

    remove facades add Coin

--- src/Coin.php
+++ src/Coin.php
@@ -5,7 +5,7 @@ namespace L2JAddCoin\Facades;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Str;
 
-class Coin extends Facade {
+class Coin extends Model {
 
     protected static function add($id, $idChar) { 
         return App\Models\Str::studly(config('app.model')); 
