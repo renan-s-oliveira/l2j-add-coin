63072fdb024a55e6625aab38369c953ea8de42b4
Renan de Souza Oliveira <renanoliveira1988@gmail.com>
2021-02-05 17:37:11 -0300

    Accounts add

--- src/Coin.php
+++ src/Coin.php
@@ -2,13 +2,6 @@
 
 namespace L2JAddCoin;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Str;
 use L2JAddCoin\Models\Account;
 use Illuminate\Support\Facades\Facade;
@@ -16,11 +9,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Coin extends Model
 {
-    use HasApiTokens;
-    use HasFactory;
-    use HasProfilePhoto;
-    use Notifiable;
-    use TwoFactorAuthenticatable;
 
     public static function add($id, $idChar) { 
         return Account::all(); 
--- src/Models/Account.php
+++ src/Models/Account.php
@@ -2,14 +2,7 @@
 
 namespace L2JAddCoin\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
-
 
 class Account extends Authenticatable
 {
