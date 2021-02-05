b4fd1790cf1322e77a46690417373c301e438862
Renan de Souza Oliveira <renanoliveira1988@gmail.com>
2021-02-05 15:44:48 -0300

    Commit

---
+++ src/Providers/L2JAddCoinServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace L2JAddCoin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class L2JAddCoinServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/l2jcoin.php' => config_path('l2jcoin.php'),
+        ]);
+    }
+}
